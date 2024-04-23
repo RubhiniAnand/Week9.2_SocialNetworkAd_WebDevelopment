d43980c60c144df30cca7d04445bdf795656794f
Rubhini Anand <144896851+RubhiniAnand@users.noreply.github.com>
2024-04-23 13:57:34 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+# Create your models here.
+class socModel(models.Model):
+
+    Age=models.FloatField()
+    EstimatedSalary=models.FloatField()
+    Gender_Male=models.FloatField()
+# Create your models here.
